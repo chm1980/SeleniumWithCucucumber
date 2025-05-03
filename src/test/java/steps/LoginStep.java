fab0c16ed6c994ff6b7651d2d61f72adabafd693
pi <chm1980@gmail.com>
2025-05-03 08:12:36 -0300

    Commit v16

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -2,52 +2,46 @@ package steps;
 
 import Base.BaseUtil;
 import com.aventstack.extentreports.GherkinKeyword;
-
 import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import pages.LoginPage;
-
 import java.util.List;
-import java.util.Map; 
-import org.junit.jupiter.api.Assertions;
+import java.util.Map;
+import org.testng.Assert;
 
 /**
  * Created by Karthik on 31/01/2019.
  */
-public class LoginStep extends BaseUtil{
+public class LoginStep extends BaseUtil {
 
-    private  BaseUtil base;
+    private BaseUtil base;
 
     public LoginStep(BaseUtil base) {
         this.base = base;
     }
 
     @DataTableType(replaceWithEmptyString = "[blank]")
-    public User convert(Map<String, String> entry){
+    public User convert(Map<String, String> entry) {
         return new User(
                 entry.get("username"),
                 entry.get("password").concat("$$$$$")
         );
     }
 
-
     @Then("^I should see the userform page$")
     public void iShouldSeeTheUserformPage() throws Throwable {
         scenarioDef.createNode(new GherkinKeyword("Then"), "I should see the userform page");
-
-        Assert.assertEquals("Its not displayed", base.Driver.findElement(By.id("Initial")).isDisplayed(), true);
+        Assert.assertEquals(base.Driver.findElement(By.id("Initial")).isDisplayed(), true, "Its not displayed");
     }
 
     @Given("^I navigate to the login page$")
     public void iNavigateToTheLoginPage() throws Throwable {
         base.scenarioDef.createNode(new GherkinKeyword("Given"), "I navigate to the login page");
-        System.out.println("Navigate Login Page");
         base.Driver.navigate().to("http://www.executeautomation.com/demosite/Login.html");
     }
-
 
     @And("^I click login button$")
     public void iClickLoginButton() throws Throwable {
@@ -56,21 +50,11 @@ public class LoginStep extends BaseUtil{
         page.ClickLogin();
     }
 
-
     @And("^I enter the following for Login$")
     public void iEnterTheFollowingForLogin(List<User> table) throws Throwable {
         base.scenarioDef.createNode(new GherkinKeyword("And"), "I enter the following for login");
-        //Create an ArrayList
-        //List<User> users =  new ArrayList<User>();
-        //Store all the users
-        //List<User> users = table.asList(User.class);
-
         LoginPage page = new LoginPage(base.Driver);
-
         page.Login(table.get(0).username, table.get(0).password);
-
-        //page.Login(users.get(2), users.get(3));
-
     }
 
     @And("^I enter ([^\"]*) and ([^\"]*)$")
@@ -82,19 +66,18 @@ public class LoginStep extends BaseUtil{
 
     @Then("^I should see the userform page wrongly$")
     public void iShouldSeeTheUserformPageWrongly() throws Throwable {
-        base.scenarioDef.createNode(new GherkinKeyword("Then"), "I should see  the useform page wrongly");
-        //Assert.assertEquals("Its not displayed", base.Driver.findElement(By.id("sdfgdsfsd")).isDisplayed(), true);
+        base.scenarioDef.createNode(new GherkinKeyword("Then"), "I should see the userform page wrongly");
+        // Código comentado de propósito
     }
-
 
     public class User {
         public String username;
         public String password;
 
         public User(String userName, String passWord) {
-            username= userName;
+            username = userName;
             password = passWord;
         }
     }
-
 }
+
