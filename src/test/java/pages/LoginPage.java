0e0abb67e7c6bc82b4eea5210e07290eae7b9bba
pi <chm1980@gmail.com>
2025-05-03 08:19:43 -0300

    Commit v17

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,60 +1,27 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
-/**
- * Página de login.
- */
 public class LoginPage {
 
     private WebDriver driver;
-    
-    // Espera explícita para elementos
-    private WebDriverWait wait;
 
+    // Construtor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        PageFactory.initElements(driver, this);
     }
 
-    @FindBy(how = How.NAME, using = "UserName")
-    public WebElement txtUserName;
-
-    @FindBy(how = How.NAME, using = "Password")
-    public WebElement txtPassword;
-
-    @FindBy(how = How.NAME, using = "Login")
-    public WebElement btnLogin;
-
-    /**
-     * Preenche os campos de login.
-     */
-    public void login(String userName, String password) {
-        try {
-            wait.until(ExpectedConditions.visibilityOf(txtUserName)).sendKeys(userName);
-            wait.until(ExpectedConditions.visibilityOf(txtPassword)).sendKeys(password);
-        } catch (Exception e) {
-            System.out.println("Erro ao preencher os campos de login: " + e.getMessage());
-        }
+    // Método que clica no botão de login
+    public void ClickLogin() {
+        driver.findElement(By.name("Login")).click();
     }
 
-    /**
-     * Clica no botão de login.
-     */
-    public void clickLogin() {
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(btnLogin)).click();
-        } catch (Exception e) {
-            System.out.println("Erro ao clicar no botão de login: " + e.getMessage());
-        }
+    // Método que preenche usuário e senha e submete
+    public void Login(String username, String password) {
+        driver.findElement(By.name("UserName")).sendKeys(username);
+        driver.findElement(By.name("Password")).sendKeys(password);
+        ClickLogin(); // reutiliza o método acima
     }
 }
 
